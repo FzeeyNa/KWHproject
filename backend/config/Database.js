f1dfdf438fb544b0ab88b950baf17331a9fe5fe2
FzeeyNa <zeerayzan@gmail.com>
2026-01-26 10:39:41 +0700

    Fix backend

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,16 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const db = new Sequelize('kwhproject', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+dotenv.config();
+
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
 export default db;
